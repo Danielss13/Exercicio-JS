af0c3b342c26eb71aea5d1b0c845e0234e264248
Danielss13 <110677780+Danielss13@users.noreply.github.com>
2022-09-05 01:40:46 -0300

    Add files via upload

---
+++ exe.js
@@ -0,0 +1,71 @@
+1//let nomeDoCarro = fusca
+
+2/*let nome = prompt ('Digite seu nome');
+alert ('Olá, ' + nome)*/
+
+3/*let nome = prompt ('Digite seu nome');
+let idade = prompt ('Qual sua idade ? ');
+alert ('Olá, ' + nome + ' Sua idade é ' + idade)*/
+
+//Retângulo
+/*let bs = prompt ('Digite a largura');
+let alt = prompt ('Digite a altura');
+let res = bs * alt
+alert (' A areá do retângulo é ' + res);*/
+
+
+                //Quadrado
+
+/*let l1 = prompt ('Digite o valor, de um dos lados do quadrado');
+let l2 = prompt ('digite o valor do outro lado');  
+let res = l1 * l2      
+alert ('A área do quadrado é ' + res)*/
+ 
+                //Losango
+
+ /*let dma = prompt ('Digite o valor da diagonal maior');
+ let dme = prompt ('Digite o valor da diagonal menor');
+ let div = 2
+ let res = dma * dme / div
+ alert ('A área do losango é ' + res);*/
+
+
+                  //Trapézio
+
+    /*let B = Number(prompt ('Digite o valor, da base maior'));
+    let b = Number(prompt ('Digite o valor da base menor'));
+    let alt = prompt ('Digite o valor da altura');
+    let div = 2
+    let res1 = (B + b) * alt /div
+    alert ('A area do trapésio é ' + res1);*/   
+    
+    
+                //paralelogramo
+
+    /*let base = prompt ('digite o valor da base');
+    let alt = prompt ('digite o valor da altura');
+    let res = base * alt
+    alert ('A area do paralelogramo ' + res);*/
+
+                //triângulo
+
+    /*let base = prompt ('digite o valor da base');
+    let alt = prompt ('digite o valor de altura');
+    let div = 2 
+    let res = base * alt / div
+    alert ('A area do triângulo é' + res);*/
+
+               // circulo
+    
+    /*let raio = prompt ('digite o raio do circulo');
+    let pi = prompt ('digite o valor de pi');
+    let res = pi * raio * raio
+    alert ('A area do circulo é' + res);*/
+
+    //essa ultima n consegui 
+    
+
+   
+
+
+
